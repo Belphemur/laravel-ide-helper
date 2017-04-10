047f15ffa598d39e3aff3c221cdacada994b05ed
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2017-04-10 11:46:32 -0400

    Revert "Fix #480 where tinyint is casts as bool (#481)"
    This reverts commit d11bf580846bd511a806ab16e4de1e89afb87b54.

--- src/Console/ModelsCommand.php
+++ src/Console/ModelsCommand.php
@@ -352,12 +352,14 @@ class ModelsCommand extends Command
                         case 'integer':
                         case 'bigint':
                         case 'smallint':
-                        case 'boolean':
                             $type = 'integer';
                             break;
                         case 'decimal':
                         case 'float':
                             $type = 'float';
+                            break;
+                        case 'boolean':
+                            $type = 'boolean';
                             break;
                         default:
                             $type = 'mixed';
